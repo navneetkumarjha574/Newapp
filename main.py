66a65d0b81325661020961e5e9831d76b0d5a46e
navneetkumarjha574 <navneetkumarjha574@gmail.com>
2026-09-07 08:29:59 +0530

    Change message sending condition to every 5 minutes

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ try:
 
         # Condition 1: Check if current minute is a multiple of 5 (0, 5, 10, 15, ..., 55)
         # Condition 2: Check if this minute hasn't already sent a message
-        if now.minute % 1 == 0 and now.minute != last_sent_minute:
+        if now.minute % 5 == 0 and now.minute != last_sent_minute:
             time_diff = target_time - now
             total_seconds = int(time_diff.total_seconds())
 
