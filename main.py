4b2a19307baca4ba3a6fc2c990ccaa4394505cd8
navneetkumarjha574 <navneetkumarjha574@gmail.com>
2026-09-14 22:53:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ IST=ZoneInfo("Asia/Kolkata")
 target_time=datetime(2026,9,16,0,0,0,tzinfo=IST)
 
 # Messages
-COUNTDOWN_TEMPLATES=["{days} D {hours} H {minutes} M Left Babu 💗"]
+COUNTDOWN_TEMPLATES=["Only {days}d {hours}h {minutes}m left, babu 💗 Counting down every second until your special day!"]
 
 def generate_birthday_message(days,hours,minutes,seconds): return random.choice(COUNTDOWN_TEMPLATES).format(days=days,hours=hours,minutes=minutes,seconds=seconds)
 
